c8b31b9778a6939b16e45319e23ea0b390c6b58e
KirAndStas <vesground@gmail.com>
2018-02-18 14:07:28 +0100

    Adding schema for tenders model

--- imports/models/tenders.js
+++ imports/models/tenders.js
@@ -1,3 +1,13 @@
 import { Mongo } from 'meteor/mongo';
 
-const Tenders = new Mongo.collections('tenders');
+const Tenders = new Mongo.Collection('tenders');
+
+Tenders.shema = new SimpleSchema({
+	id       : {type: String},
+	title    : {type: String},
+	location : {type: String},
+	price    : {type: Number},
+	currency : {type: String}
+});
+
+export default Tenders;
