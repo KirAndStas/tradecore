7a63f7b755535baa08aedf08a303f2337f9a1137
KirAndStas <vesground@gmail.com>
2018-02-18 14:08:56 +0100

    Fix

--- server/services/insertTender.js
+++ server/services/insertTender.js
@@ -1,8 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 
-import '../../imports/models/tenders.js';
-
-const Tenders = Mongo.Collection.get('tenders');
+import Tenders  from '../../imports/models/tenders.js';
 
 export default function insertTender(tender) {
 	const { id, title, location, currency, price } = tender;
